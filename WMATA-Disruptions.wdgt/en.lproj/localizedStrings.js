e1b8bb6200e7449beaad77937497bf62505f6c90
macksold <macksold@e9491e59-8d34-0410-a9f3-618a8d5f81c9>
2007-07-06 15:27:30 +0000

    Initial import

---
+++ WMATA-Disruptions.wdgt/en.lproj/localizedStrings.js
@@ -0,0 +1,18 @@
+var localizedStrings = new Object;
+
+localizedStrings["My RSS Feed"] = "My RSS Feed";
+localizedStrings["New URL: "] = "New URL: ";
+localizedStrings["Set"] = "Set";
+localizedStrings["Feed URLs: "] = "Feed URLs: ";
+localizedStrings["Article Length"] = "Article Length";
+localizedStrings["Reset"] = "Reset";
+localizedStrings["Done"] = "Done";
+localizedStrings["Invalid Feed"] = "Invalid Feed";
+localizedStrings["%s does not appear to be a valid RSS or Atom feed."] = "%s does not appear to be a valid RSS or Atom feed.";
+localizedStrings["No Items Found"] = "No Items Found";
+localizedStrings["The feed does not contain any entries."] = "The feed does not contain any entries.";
+localizedStrings["No items matched the search terms."] = "No items matched the search terms.";
+localizedStrings["Nothing To Display"] = "Nothing To Display";
+localizedStrings["The feed does not contain any entries within the specified criteria."] = "The feed does not contain any entries within the specified criteria.";
+localizedStrings["Loading"] = "Loading";
+localizedStrings["%s new"] = "%s new";
